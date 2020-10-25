33fef3cf75f57bdb9698dc1cea82873f2cff8193
arthur <hauton.arthur@gmail.com>
2020-10-25 14:36:33 +0100

    Init Push

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Monobehaviours
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        [SerializeField] private float look_sensivity;
+        [SerializeField] private float _smoothing;
+
+        private GameObject _player;
+        private Vector2 smoothVelocity;
+        private Vector2 currentLooking;
+    
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _player = transform.parent.gameObject;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            RotateCamera();
+        }
+
+        private void RotateCamera()
+        {
+            Vector2 inputValues = new Vector2(Input.GetAxisRaw("Mouse X"),0f);
+            inputValues = Vector2.Scale(inputValues, new Vector2(look_sensivity * _smoothing, look_sensivity * _smoothing));
+        
+            smoothVelocity.x = Mathf.Lerp(smoothVelocity.x, inputValues.x, 1f / _smoothing);
+            smoothVelocity.y = Mathf.Lerp(smoothVelocity.y, inputValues.y, 1f / _smoothing);
+
+            currentLooking += smoothVelocity;
+        
+            transform.localRotation = Quaternion.AngleAxis(-currentLooking.y, Vector3.right);
+            _player.transform.localRotation = Quaternion.AngleAxis(currentLooking.x, _player.transform.up);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+    public class PlayerControls : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+
+        private Rigidbody rb;
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        } 
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, 0, vertical) * (_speed * Time.deltaTime);
+
+            Vector3 newPosition = rb.position + rb.transform.TransformDirection(movement);
+            rb.MovePosition(newPosition);
+        }
+
+    }
