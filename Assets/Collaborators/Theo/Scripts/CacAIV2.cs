c25534ab0cfbf3b3c8132772199a5ff18b682824
Drevok <theoartous@hotmail.fr>
2020-11-30 15:01:09 +0100

    Continued clear melee

--- Assets/Collaborators/Theo/Scripts/CacAIV2.cs
+++ Assets/Collaborators/Theo/Scripts/CacAIV2.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CacAIV2 : MonoBehaviour
+public class CacAIV2 : MonoBehaviour, IDamageable
 {
     private NavMeshAgent _navMeshAgent;
     private GameObject _player;
@@ -11,11 +12,129 @@ public class CacAIV2 : MonoBehaviour
     private float _enemySpeedBack = -6f;
     private float _enemyStoppedSpeed = 0f;
 
+    private Animator _animator;
+
     public GameObject pointe;
 
     private int _maxHealth = 80;
     private int _currentHealth;
-    
-    private bool _playerIsInMeleeRange => Vector3.Distance(transform.position, _player.transform.position)
 
+    private bool _playerIsInMeleeRange => Vector3.Distance(transform.position, _player.transform.position) < 2;
+    private bool _playerIsEnoughFar => Vector3.Distance(transform.position, _player.transform.position) > 10;
+
+    public bool isGrabbed;
+    private bool _hasAttacked;
+
+    private State _currentState;
+
+    public bool _playerIsInSight;
+
+    private enum State
+    {
+        Idle,
+        Chasing,
+        Melee,
+        Flee,
+        Hooked,
+        Dead
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+        _animator = GetComponent<Animator>();
+        _animator.SetBool("isIdle", true);
+        isGrabbed = false;
+        _hasAttacked = false;
+    }
+
+    private void Update()
+    {
+        switch (_currentState)
+        {
+            case State.Idle:
+                if (_playerIsInSight)
+                {
+                    _currentState = State.Chasing;
+                }
+                else if (_haveMeleeAttack && _playerIsInMeleeRange)
+                {
+                    _currentState = State.Melee;
+                }
+                else if (_playerIsInSight)
+                {
+                    _currentState = State.Walking;
+                }
+                
+                else if (isGrabbed)
+                {
+                    _currentState = State.Hooked;
+                }
+                break;
+                break;
+            case State.Chasing:
+                break;
+            case State.Melee:
+                return;
+            case State.Flee:
+                return;
+            case State.Hooked:
+                return;
+            case State.Dead:
+                return;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            _currentState = State.Dead;
+        }
+
+        if (isGrabbed)
+        {
+            _currentState = State.Hooked;
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        Debug.Log("PAF");
+    }
+
+    private void ChasePlayer()
+    {
+        _navMeshAgent.speed = _enemySpeed;
+        Vector3 dirToPlayer = transform.position - _player.transform.position;
+        Vector3 newPos = transform.position + dirToPlayer;
+        _navMeshAgent.SetDestination(newPos);
+    }
+
+    private void Flee()
+    {
+        float distance = Vector3.Distance(transform.position, _player.transform.position);
+        _navMeshAgent.speed = _enemySpeedBack;
+        Vector3 dirToPlayer = transform.position - _player.transform.position;
+        Vector3 newPos = transform.position + dirToPlayer;
+        _navMeshAgent.SetDestination(newPos);
+
+    }
+
+    private void Harpooned(Transform col)
+    {
+        transform.position = col.transform.position;
+    }
+
+    public void Die()
+    {
+        Debug.Log("J'AI MAAAAAAAAAAAAAAAAL");
+        _navMeshAgent.speed = _enemyStoppedSpeed;
+        _animator.SetBool("isDead", true);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _currentHealth = _currentHealth - amount;
+    }
 }
